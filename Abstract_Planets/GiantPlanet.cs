7b248a00fd34ad40396636357f2258a820391551
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-07-22 15:25:49 -0400

    Added a TerrestrialPlanet sub class

--- Abstract_Planets/GiantPlanet.cs
+++ Abstract_Planets/GiantPlanet.cs
@@ -13,10 +13,19 @@ namespace Abstract_Planets
     // This is the Giant Sub clas
     public class GiantPlanet : Planet
     {
+        
         //Private Instance Varibles
+        //This field, _type, refers to what type of GiantPlanet it is
         private string _type;
 
-
+        /// <summary>
+        /// This is the contructer of the the Giant Planet
+        /// It take four arguments.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="type"></param>
         //Contructer
         public GiantPlanet(string name, double diameter, double mass, string type)
             :base(name, diameter,mass)
---
+++ Abstract_Planets/TerrestrialPlanet.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 22, 2017
+ * Description: This is a TesrrestrialPlanet sub class which derived from Planet Class
+ * Version: 0.2 Instance variable, Contructer, and Methods
+ */
+namespace Abstract_Planets
+{
+    //This is the TerrestrialPlanet Class
+    public class TerrestrialPlanet : Planet
+    {
+        //Private Instance Variable
+        // This field, _oxygen, refer to the Planet if is has oxygen
+        private bool _oxygen;
+        
+        //Constructer
+        /// <summary>
+        /// This this the Contructer of the Terrestrial Class
+        /// It takes four aruguments.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="oxygen"></param>
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+            :base(name,diameter,mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+
+        //Public Methods
+        // This is a Boolean method that return a 
+        // true value if the Planets has moon/s
+        public bool HasMoons()
+        {
+            bool greaterThanZero = false;
+            if (MoonCount > 0)
+                greaterThanZero = true;
+            return greaterThanZero;
+
+        }
+        //This is a boolean method that a true
+        //value if the Planet is Habitable
+        public bool Habitable()
+        {
+            bool habitable = false;
+            if (_oxygen == true)
+                habitable = true;
+            return habitable;
+          
+        }
+
+
+
+    }
+}
