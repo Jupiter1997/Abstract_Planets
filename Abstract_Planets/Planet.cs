9b95b88ab51d398aa1e1204f3e27db1567b25d33
Mr. Marvin Jupiter Roilo Vargas <mvargas2@my.centennialcollege.ca>
2017-07-22 14:18:52 -0400

    Planet Class created

---
+++ Abstract_Planets/Planet.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 22, 2017
+ * Description: This is the Planet super class
+ * Version: 0.1 Planet class created with fields and properties
+ */
+namespace Abstract_Planets
+{
+    public abstract class Planet
+    {
+        //private instance variables
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotatiobPeriod;
+
+
+        //public properties
+        public double Diameter
+        {
+            get
+            {
+                return _diameter;
+            }
+        }
+        public double Mass
+        {
+            get
+            {
+                return _mass;
+
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public int MoonCount
+        {
+            get
+            {
+                return _moonCount;
+            }
+            set
+            {
+                _moonCount = value;
+            }
+        }
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return _orbitalPeriod;
+            }
+            set
+            {
+                _orbitalPeriod = value;
+            }
+        }
+        public int RingCount
+        {
+            get
+            {
+                return _ringCount;
+            }
+            set
+            {
+                _ringCount = value;
+            }
+        }
+        public double RotationPeriod
+        {
+            get
+            {
+                return _ringCount;
+            }
+            set
+            {
+                _rotatiobPeriod = value;
+            }
+        }
+        //constructer
+
+        //private methods
+
+        //public methods
+    }
+}
--- Abstract_Planets/Program.cs
+++ Abstract_Planets/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Name: Marvin Jupiter Vargas
+ * Date: July 22, 2017
+ * Description: This is and example of using abstarct class by creating an Abstract Planets Project
+ * Version: 0.1 Class Diagram created
+ */
 namespace Abstract_Planets
 {
     class Program
